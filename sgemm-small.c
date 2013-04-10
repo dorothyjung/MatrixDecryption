99d424adb81efca72be65d87e5b5ec1a102e92e8
yami280 <jeneyuenli@gmail.com>
2013-04-09 20:27:03 -0700

    sse implemented

--- sgemm-small.c
+++ sgemm-small.c
@@ -4,8 +4,14 @@
 
 void sgemm( int m, int n, int d, float *A, float *C )
 {
-  for( int i = 0; i < n; i++ )
-    for( int k = 0; k < m; k++ ) 
-      for( int j = 0; j < n; j++ ) 
-	C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
+	for(int j = 0; j < n; j = j + 4) {
+		for(int k = 0; k < m; k = k + 4) {
+			for(int i = 0; i < n; i = i + 4) {
+				__m128 result = _mm_mul_ps(_mm_loadu_ps(A+i+k*n), _mm_loadu_ps(A+j*(n+1)+k*n));
+				result = _mm_add_ps(_mm_loadu_ps(C+i+j*n), result);
+				_mm_storeu_ps(C+i+j*n, result);
+			}
+		}
+	}
 }
+
