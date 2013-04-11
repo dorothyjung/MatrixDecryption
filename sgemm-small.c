4f18f9bd8b1a0fe9175220b8de1bf428ff640c4d
yami280 <jeneyuenli@gmail.com>
2013-04-11 00:46:19 -0700

    no change

--- sgemm-small.c
+++ sgemm-small.c
@@ -4,8 +4,8 @@
 
 void sgemm( int m, int n, int d, float *A, float *C )
 {
-	for(int j = 0; j < n; j = j + 4) {
-		for(int k = 0; k < m; k = k + 4) {
+	for(int j = 0; j < n; j = j++) {
+		for(int k = 0; k < m; k++) {
 			for(int i = 0; i < n; i = i + 4) {
 				__m128 result = _mm_mul_ps(_mm_loadu_ps(A+i+k*n), _mm_loadu_ps(A+j*(n+1)+k*n));
 				result = _mm_add_ps(_mm_loadu_ps(C+i+j*n), result);
