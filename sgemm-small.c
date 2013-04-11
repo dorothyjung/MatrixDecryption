1cb163baa9fd65a1454aee42a12a646ae3751cfd
yami280 <jeneyuenli@gmail.com>
2013-04-11 01:21:25 -0700

    added code to create padded buffer

--- sgemm-small.c
+++ sgemm-small.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <nmmintrin.h>
+#include <string.h>
 
 void sgemm( int m, int n, int d, float *A, float *C )
 {
+	if (n % 4 != 0) {
+		// pad matrix with 0s if not divisible by 4 
+		float buffer[(n + n % 4) * (n + d)] = {0};
+		for (int i = 0; i < d+n; i++) {
+			memcpy(buffer + i*(n + n % 4), A + i*n, n);
+		}
+		A = buffer;
+	}
 	for(int j = 0; j < n; j = j++) {
 		for(int k = 0; k < m; k++) {
 			for(int i = 0; i < n; i = i + 4) {
