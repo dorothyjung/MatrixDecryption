ed708426489a5b63f3839f74a2db4d3ce8f28e18
yami280 <jeneyuenli@gmail.com>
2013-04-11 01:43:59 -0700

    bugfix3

--- sgemm-small.c
+++ sgemm-small.c
@@ -10,13 +10,13 @@ void sgemm( int m, int n, int d, float *A, float *C )
 	float *temp;
 	if ((n % ROLL_SIZE) != 0) {
 		// pad matrix with 0s if not divisible by 4 
-		float buffer[(n + n % ROLL_SIZE) * (n + d)] = {0};
+		float buffer[(n + n % ROLL_SIZE) * (n + d)];
 		for (int i = 0; i < d+n; i++) {
 			memcpy(buffer + i*(n + n % ROLL_SIZE), A + i*n, n);
 		}
 		A = buffer;
 		temp = C;
-		float cbuffer[(n + n % ROLL_SIZE) * (n + n % ROLL_SIZE)] = {0};
+		float cbuffer[(n + n % ROLL_SIZE) * (n + n % ROLL_SIZE)];
 		C = cbuffer;
 	}
 	for(int j = 0; j < n; j = j++) {
