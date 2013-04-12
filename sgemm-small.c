a0e7f05ec3b8c0963e218e462db31a7e2b4c0c7d
yami280 <jeneyuenli@gmail.com>
2013-04-11 19:10:13 -0700

    fix

--- sgemm-small.c
+++ sgemm-small.c
@@ -22,7 +22,7 @@ void sgemm( int m, int n, int d, float *A, float *C )
 	for(int j = 0; j < n; j = j++) {
 		for(int k = 0; k < m; k++) {
 			for(int i = 0; i < n; i = i + ROLL_SIZE) {
-				__m128 result = _mm_mul_ps(_mm_loadu_ps(A+i+k*n), _mm_loadu_ps(A+j*(n+1)+k*n));
+				__m128 result = _mm_mul_ps(_mm_loadu_ps(A+i+k*n), _mm_load1_ps(A+j*(n+1)+k*n));
 				result = _mm_add_ps(_mm_loadu_ps(C+i+j*n), result);
 				_mm_storeu_ps(C+i+j*n, result);
 			}
