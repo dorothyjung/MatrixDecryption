032e54984fb0e83690530d05fae8a78a83302179
Dorothy Jung <jungy@berkeley.edu>
2013-04-11 18:25:04 -0700

    printMatrix

--- sgemm-small.c
+++ sgemm-small.c
@@ -42,8 +42,15 @@ void printMatrix(int n, int m, float *A) {
 		matrixString += "=";
 	}
 	matrixString += "\n";
-	for (int i = 0; i < m; i++) {
-		
+	for (int i = 0; i < n; i++) {
+	    for (int j = 0; j < m; j ++) {
+		matrixString += A[n*j+i];
+	    }
+	    matrixString += "\n";
 	}
+	for (int i = 0; i < m ; i++) {
+		matrixString += "=";
+	}
+	matrixString += "\n";
 
 }
