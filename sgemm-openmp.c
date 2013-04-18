6f589279a058d47e09e2a97da6080a8d9c1ad4f6
yami280 <yami280@berkeley.edu>
2013-04-17 18:40:23 -0700

    added skeleton for parallel

---
+++ sgemm-openmp.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <nmmintrin.h>
+#include <string.h>
+#include <omp.h>
+
+void sgemm(int m, int n, int d, float *A, float *C) {
+    
+    for (k = 0; k < m; k++) {
+        for (j = 0; j < n; j++) {
+            for (i = 0; i < n; i++) {
+                
+            }
+        }
+    }
+}
