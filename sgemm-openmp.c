97ed4d4eda99105a328dce4e7483cda2204e053c
yami280 <yami280@berkeley.edu>
2013-04-21 23:25:44 -0700

    minor optimizatoins

--- sgemm-openmp.c
+++ sgemm-openmp.c
@@ -7,16 +7,17 @@
 #define VERTICAL_ROLL 16
 #define HORIZONTAL_ROLL 4
 #define MIN(X, Y) (X<Y ? X : Y)
-#define BLOCKSIZE 16
+#define BLOCKSIZE 32
 
 void sgemm( int m, int n, int d, float *A, float *C )
 {
     int n1 = n+1;
+    int nEnd = n/VERTICAL_ROLL*VERTICAL_ROLL;
 	#pragma omp parallel for
     for (int j = 0; j < n; j++) {
     	for (int b = 0; b < m; b+= BLOCKSIZE) {
 			int jn1 = j*(n+1), jn = j*n; float *Cjn = C+jn;
-			for (int i = 0; i < n/VERTICAL_ROLL*VERTICAL_ROLL; i+=VERTICAL_ROLL) {
+			for (int i = 0; i < nEnd; i+=VERTICAL_ROLL) {
 			    int i1 = i + 4;
 			    int i2 = i + 8;
 			    int i3 = i + 12;
@@ -50,22 +51,24 @@ void sgemm( int m, int n, int d, float *A, float *C )
     if (n % VERTICAL_ROLL != 0 && (n - (n/VERTICAL_ROLL*VERTICAL_ROLL) >= 4)) {
 		#pragma omp parallel for
 		for (int j = 0; j < n; j++) {
-			for (int i = n/VERTICAL_ROLL*VERTICAL_ROLL; i < n/4*4; i+=4) {
-				float *addrCij = C+i+j*n;
-				float *Ajn1 = A+j*n1;
-				float *Ai = A+i;
-				__m128 Cij = _mm_loadu_ps(addrCij);
-				for (int k = 0; k < m; k++) {
-				    int kn = k*n;				    
-				    __m128 Ajk = _mm_load1_ps(Ajn1+k*n);
-				    __m128 Aik = _mm_loadu_ps(Ai+k*n);
-				    Cij = _mm_add_ps(Cij, _mm_mul_ps(Ajk, Aik));
+			for (int b = 0; b < m; b+= BLOCKSIZE) {
+				for (int i = nEnd; i < n/4*4; i+=4) {
+					float *addrCij = C+i+j*n;
+					float *Ajn1 = A+j*n1;
+					float *Ai = A+i;
+					__m128 Cij = _mm_loadu_ps(addrCij);
+					for (int k = b; k < MIN(m, b+BLOCKSIZE); k++) {
+					    int kn = k*n;				    
+					    __m128 Ajk = _mm_load1_ps(Ajn1+k*n);
+					    __m128 Aik = _mm_loadu_ps(Ai+k*n);
+					    Cij = _mm_add_ps(Cij, _mm_mul_ps(Ajk, Aik));
+					}
+					_mm_storeu_ps(addrCij, Cij);
 				}
-				_mm_storeu_ps(addrCij, Cij);
 			}
 		}
     }
-    if ((n - n/VERTICAL_ROLL*VERTICAL_ROLL) % 4 != 0) {
+    if ((n - nEnd) % 4 != 0) {
 		#pragma omp parallel for
 		for (int j = 0; j < n; j++) {
 		    float *Ajn1 = A+j*n1;
