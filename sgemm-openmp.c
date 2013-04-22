934e34d9866105ac6410e81e53104a1820e1ad7d
yami280 <yami280@berkeley.edu>
2013-04-21 23:20:48 -0700

    added cacheblocking

--- sgemm-openmp.c
+++ sgemm-openmp.c
@@ -7,41 +7,44 @@
 #define VERTICAL_ROLL 16
 #define HORIZONTAL_ROLL 4
 #define MIN(X, Y) (X<Y ? X : Y)
+#define BLOCKSIZE 16
 
 void sgemm( int m, int n, int d, float *A, float *C )
 {
     int n1 = n+1;
 	#pragma omp parallel for
     for (int j = 0; j < n; j++) {
-		int jn1 = j*(n+1), jn = j*n; float *Cjn = C+jn;
-		for (int i = 0; i < n/VERTICAL_ROLL*VERTICAL_ROLL; i+=VERTICAL_ROLL) {
-		    int i1 = i + 4;
-		    int i2 = i + 8;
-		    int i3 = i + 12;
-		    
-		    __m128 Cij = _mm_loadu_ps(Cjn+i);
-		    __m128 Cij1 = _mm_loadu_ps(Cjn+i1);
-		    __m128 Cij2 = _mm_loadu_ps(Cjn+i2);
-		    __m128 Cij3 = _mm_loadu_ps(Cjn+i3);
-		    
-		    for (int k = 0; k < m; k++) {
-				int k1 = k + 1; float *Akn = A+k*n;
-				__m128 Ajk = _mm_load1_ps(Akn+jn1);
-				
-				__m128 Aik = _mm_loadu_ps(Akn+i);
-				__m128 Ai1k = _mm_loadu_ps(Akn+i1);
-				__m128 Ai2k = _mm_loadu_ps(Akn+i2);
-				__m128 Ai3k = _mm_loadu_ps(Akn+i3);
-				
-				Cij = _mm_add_ps(Cij, _mm_mul_ps(Ajk, Aik));
-				Cij1 = _mm_add_ps(Cij1, _mm_mul_ps(Ajk, Ai1k));
-				Cij2 = _mm_add_ps(Cij2, _mm_mul_ps(Ajk, Ai2k));
-				Cij3 = _mm_add_ps(Cij3, _mm_mul_ps(Ajk, Ai3k));
-		    }
-		    _mm_storeu_ps(Cjn+i, Cij);
-		    _mm_storeu_ps(Cjn+i1, Cij1);
-		    _mm_storeu_ps(Cjn+i2, Cij2);
-		    _mm_storeu_ps(Cjn+i3, Cij3);
+    	for (int b = 0; b < m; b+= BLOCKSIZE) {
+			int jn1 = j*(n+1), jn = j*n; float *Cjn = C+jn;
+			for (int i = 0; i < n/VERTICAL_ROLL*VERTICAL_ROLL; i+=VERTICAL_ROLL) {
+			    int i1 = i + 4;
+			    int i2 = i + 8;
+			    int i3 = i + 12;
+			    
+			    __m128 Cij = _mm_loadu_ps(Cjn+i);
+			    __m128 Cij1 = _mm_loadu_ps(Cjn+i1);
+			    __m128 Cij2 = _mm_loadu_ps(Cjn+i2);
+			    __m128 Cij3 = _mm_loadu_ps(Cjn+i3);
+			    
+			    for (int k = b; k < MIN(m, b+BLOCKSIZE); k++) {
+					int k1 = k + 1; float *Akn = A+k*n;
+					__m128 Ajk = _mm_load1_ps(Akn+jn1);
+					
+					__m128 Aik = _mm_loadu_ps(Akn+i);
+					__m128 Ai1k = _mm_loadu_ps(Akn+i1);
+					__m128 Ai2k = _mm_loadu_ps(Akn+i2);
+					__m128 Ai3k = _mm_loadu_ps(Akn+i3);
+					
+					Cij = _mm_add_ps(Cij, _mm_mul_ps(Ajk, Aik));
+					Cij1 = _mm_add_ps(Cij1, _mm_mul_ps(Ajk, Ai1k));
+					Cij2 = _mm_add_ps(Cij2, _mm_mul_ps(Ajk, Ai2k));
+					Cij3 = _mm_add_ps(Cij3, _mm_mul_ps(Ajk, Ai3k));
+			    }
+			    _mm_storeu_ps(Cjn+i, Cij);
+			    _mm_storeu_ps(Cjn+i1, Cij1);
+			    _mm_storeu_ps(Cjn+i2, Cij2);
+			    _mm_storeu_ps(Cjn+i3, Cij3);
+			}
 		}
     }
     if (n % VERTICAL_ROLL != 0 && (n - (n/VERTICAL_ROLL*VERTICAL_ROLL) >= 4)) {
